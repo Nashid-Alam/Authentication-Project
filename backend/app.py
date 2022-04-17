085925b28ffa183b04287ea33970c9aeeb5b7ae7
Nashid-Alam <nashid17@gmail.com>
2022-04-17 11:00:13 -0400

    succesfully submitted page

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask,render_template,request
 
 app = Flask (__name__)
@@ -5,6 +6,19 @@ app = Flask (__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+    if request.method == 'POST':
+        first_name = request.form['first name']
+        last_name = request.form['last name']
+        username = request.form['username']
+        password = request.form['password']
+
+        print(first_name,last_name,username,password)
+
+        return render_template('success.html')
+
 
 
 if __name__ == '__main__':
